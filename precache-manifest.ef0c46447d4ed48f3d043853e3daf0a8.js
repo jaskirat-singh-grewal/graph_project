39bcc5c194af692ae88353ec4031b179b8c98842
Jaskirat Grewal <jaskiratgrewal@Jaskirats-MBP.hitronhub.home>
2020-07-18 18:34:44 -0500

    Updates

--- precache-manifest.ef0c46447d4ed48f3d043853e3daf0a8.js
+++ precache-manifest.ef0c46447d4ed48f3d043853e3daf0a8.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "71c2274d5ad0ee45f8bb846551dbd37c",
+    "revision": "03d60788090fc669d7a33c8db47eb3be",
     "url": "/graph_project/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/graph_project/static/css/2.829c9cb5.chunk.css"
   },
   {
-    "revision": "9f695c186717069249e8",
-    "url": "/graph_project/static/css/main.3a83531a.chunk.css"
+    "revision": "d67a6b1d74729e799bb9",
+    "url": "/graph_project/static/css/main.b4da9ead.chunk.css"
   },
   {
     "revision": "57fd5eea5cb8b0d84675",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/graph_project/static/js/2.e3da1d01.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9f695c186717069249e8",
-    "url": "/graph_project/static/js/main.a3ae35dc.chunk.js"
+    "revision": "d67a6b1d74729e799bb9",
+    "url": "/graph_project/static/js/main.265d86da.chunk.js"
   },
   {
     "revision": "d6e28d123fe468a5260b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/graph_project/precache-manifest.358b9f41778dd15dd3d7ee6087aed322.js"
+  "/graph_project/precache-manifest.ef0c46447d4ed48f3d043853e3daf0a8.js"
 );
 
 self.addEventListener('message', (event) => {
