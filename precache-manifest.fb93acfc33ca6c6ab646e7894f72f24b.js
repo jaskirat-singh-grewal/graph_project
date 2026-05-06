5b508f5b393a93e4e0b24bd35d0b14204bde2fb1
Megatron <megatron.j41@gmail.com>
2026-05-06 23:09:26 +0000

    deploy: Phase 1 — algorithm selector dropdown

---
+++ precache-manifest.fb93acfc33ca6c6ab646e7894f72f24b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b215465b423f39a81da788491f899831",
+    "url": "/graph_project/index.html"
+  },
+  {
+    "revision": "3c051cc3661414fe9857",
+    "url": "/graph_project/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "f8ab1b02e3a3c363db8b",
+    "url": "/graph_project/static/css/main.578d3b2f.chunk.css"
+  },
+  {
+    "revision": "3c051cc3661414fe9857",
+    "url": "/graph_project/static/js/2.93784476.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/graph_project/static/js/2.93784476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8ab1b02e3a3c363db8b",
+    "url": "/graph_project/static/js/main.a4586f6f.chunk.js"
+  },
+  {
+    "revision": "d6e28d123fe468a5260b",
+    "url": "/graph_project/static/js/runtime-main.512ceb70.js"
+  }
+]);
